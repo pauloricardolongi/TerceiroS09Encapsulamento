633f9ebeb820560d2dfed61c21595d154160689e
pauloricardolongi <pauloricardolongi@gmail.com>
2021-11-06 16:57:39 -0300

    construtor padrao

--- src/application/Program.java
+++ src/application/Program.java
@@ -18,23 +18,24 @@ public class Program {
 		System.out.print("Price: ");
 		double price = sc.nextDouble();		
 		
-		Product product = new Product(name, price);	
+		//Product product = new Product(name, price);
+		Product p = new Product();
 		
 		
 		System.out.println();
-		System.out.println("Product data: " +  product);
+		System.out.println("Product data: " +  p);
 		System.out.println();
 		System.out.print("Enter the number of products to be added in stock: ");
 	    int quantity = sc.nextInt();
-		product.addProducts(quantity);
+		 p.addProducts(quantity);
 		System.out.println();
-		System.out.println("Updated data: " + product);
+		System.out.println("Updated data: " + p);
 		System.out.println();
 		System.out.print("Enter the number of products to be removed from stock: ");
 		quantity = sc.nextInt();
-		product.removeProducts(quantity);
+		p.removeProducts(quantity);
 		System.out.println();
-		System.out.println("Updated data: " + product);
+		System.out.println("Updated data: " + p);
 		sc.close();
 	}
 }
